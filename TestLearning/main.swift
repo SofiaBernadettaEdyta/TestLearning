73a8a60d0084fcc0741923a03e09fcf7e3c3cb01
Zofia Drabek <zosia.drabek@gmail.com>
2020-02-09 12:08:25 +0100

    Initial Commit

---
+++ TestLearning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestLearning
+//
+//  Created by Zofia Drabek on 09/02/2020.
+//  Copyright © 2020 Zofia Drabek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
