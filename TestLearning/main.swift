e28f49072e6d9cd35fbdb20bf7bb8e8c0004519a
Zofia Drabek <zosia.drabek@gmail.com>
2020-06-17 16:28:37 +0200

    Experiment with tests.

--- TestLearning/main.swift
+++ TestLearning/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+func add(a: Int, b: Int) -> Int {
+    return a + b
+}
 
+func testAddPositives() -> Bool {
+    return add(a: 3, b: 7) == 10
+}
+
+func testAddNegatives() -> Bool {
+    return add(a: -10, b: -3) == -13
+}
+
+func testAddOposites() -> Bool {
+    return add(a: -3, b: 3) == 0
+}
+
+func testReflecsivity() -> Bool{
+    let result0 = add(a: 3, b: 5)
+    let result1 = add(a: 5, b: 3)
+    let check0 = result0 == 8
+    let check1 = result1 == 8
+    return check0 && check1
+}
+
+//func testAddMax() -> Bool {
+//    return add(a: Int.max, b: 1) == 0
+//}
+
+let testArray = [
+    testAddPositives,
+    testAddNegatives,
+    testAddOposites,
+//    testAddMax
+]
+
+let testPassed = !testArray
+    .map { $0() }
+    .contains(false)
+
+print("test is passed \(testPassed)")
